f0746f28e13daa8418d0e0441c3b7729b778a287
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2023-04-09 10:30:46 +0900

    いらん

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
-import deno from "@astrojs/deno";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: [vercel(), deno()],
+  adapter: vercel(),
 });
