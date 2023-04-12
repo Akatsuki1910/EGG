0cf80f2eeabcf55bd628eee646ff73bac3267d7c
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2023-04-13 01:00:35 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  // adapter: vercel(),
-  site: "https://akatsuki1910.github.io/",
-  base: "egg",
+  output: "server",
+  adapter: vercel(),
+  // site: "https://akatsuki1910.github.io/",
+  // base: "egg",
 });
